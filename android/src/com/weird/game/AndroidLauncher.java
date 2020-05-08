da201d9aba6d74b59b3e0b4423ea3be9c312c61f
Luis Frontanilla <lrdolucho@gmail.com>
2020-05-08 04:46:45 -0400

    Initial commit

---
+++ android/src/com/weird/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.weird.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.weird.game.WeirdGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WeirdGame(), config);
+	}
+}
